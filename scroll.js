9a1c503278284a9741853ba1944b81fa532d66e5
Hannah-U <girlswhocode@Girls-Who-Code-8.local>
2015-08-20 11:26:01 -0400

    Made Content Edits
    Changed mission statement; added clocks; changed arrow; added social
    media links.

--- scroll.js
+++ scroll.js
@@ -86,20 +86,3 @@ $(window).scroll( function(){
   $('.arrow-wrap').css('opacity', position);
 
 });
-
-
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
-
